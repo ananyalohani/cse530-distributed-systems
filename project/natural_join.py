000ed3e150bc5e35a81d1bb8fe29a9e63aa6df1b
Ananya Lohani <ananya19018@iiitd.ac.in>
2023-04-30 03:35:31 +0530

    fix: Add generic cartesian product

--- project/natural_join.py
+++ project/natural_join.py
@@ -1,8 +1,11 @@
-from functools import reduce
-import os, time, pandas, csv, json
+import csv
+import itertools
+import os
+import time
 
-from map_reduce import Manager, Mapper, Reducer
 import map_reduce_pb2
+import pandas
+from map_reduce import Manager, Mapper, Reducer
 
 
 class NaturalJoinMapper(Mapper):
@@ -20,9 +23,7 @@ class NaturalJoinMapper(Mapper):
             tables.append(table)
 
         # * Assumption: Only one common column
-        common_col = list(
-            reduce(lambda x, y: set(x.columns).intersection(y.columns), file_dfs)
-        )[0]
+        common_col = list(set.intersection(*[set(df.columns) for df in file_dfs]))[0]
         for i, df in enumerate(file_dfs):
             columns = df.columns.values
             for j, row in df.iterrows():
@@ -34,7 +35,11 @@ class NaturalJoinMapper(Mapper):
         return map_reduce_pb2.MapResponse(filepaths=list(self.shard_filepaths))
 
     def _map(self, key, value):
-        shard_index = len(key[0]) % self.num_reducers
+        shard_index = 0
+        if type(key[0]) == str:
+            shard_index = len(key[0]) % self.num_reducers
+        elif type(key[0]) == int:
+            shard_index = key[0] % self.num_reducers
         idx = shard_index + 1
         filepath = os.path.join(
             os.path.dirname(__file__),
@@ -47,30 +52,29 @@ class NaturalJoinMapper(Mapper):
 
 class NaturalJoinReducer(Reducer):
     def Reduce(self, request, context):
-        # TODO: grpc call to get file data
-        filepath = request.filepath
         keys = []
         values = []
         cols = set()
         tables = set()
 
         # Read from intermediate file
-        with open(filepath, "r") as f:
-            for line in f:
-                key, value = line.strip().split(") (")
-                key = tuple(k.strip("'") for k in key.strip("()").split(", "))
-                value = tuple(v.strip("'") for v in value.strip("()").split(", "))
-                cols.add(value[2])
-                tables.add(value[0])
-                if key not in keys:
-                    keys.append(key)
-                    values.append([value])
-                else:
-                    values[keys.index(key)].append(value)
+        file_content = self.get_intermediate_file(
+            mapper_address=request.mapper_address, filepath=request.filepath
+        )
+        for line in file_content.split("\n"):
+            key, value = line.strip().split(") (")
+            key = tuple(k.strip("'") for k in key.strip("()").split(", "))
+            value = tuple(v.strip("'") for v in value.strip("()").split(", "))
+            cols.add(value[2])
+            tables.add(value[0])
+            if key not in keys:
+                keys.append(key)
+                values.append([value])
+            else:
+                values[keys.index(key)].append(value)
 
         # Write csv header to output file
         cols = list(sorted(cols))
-        print(cols)
         cols.insert(0, keys[0][1])
         with open(
             os.path.join(
@@ -100,22 +104,18 @@ class NaturalJoinReducer(Reducer):
             table_values[idx][key[1]] = key[0]
             for v in value:
                 if v[2] not in table_values[idx]:
-                    table_values[idx][v[2]] = []
-                table_values[idx][v[2]].append(v)
+                    table_values[idx][v[2]] = set()
+                table_values[idx][v[2]].add(v)
+            for v in value:
+                table_values[idx][v[2]] = list(table_values[idx][v[2]])
 
         # Get cartesian product of values for each key
         final_rows = [{"key": key, "values": []} for key in new_keys]
         for i, row in enumerate(table_values):
             keys = list(row.keys())
-            for j in range(len(keys)):
-                if keys[j] == cols[0]:
-                    continue
-                for k in range(j + 1, len(keys)):
-                    if keys[k] == cols[0]:
-                        continue
-                    for v1 in row[keys[j]]:
-                        for v2 in row[keys[k]]:
-                            final_rows[i]["values"].append((v1, v2))
+            keys.remove(new_keys[0][1])
+            prod = list(itertools.product(*[row[k] for k in keys]))
+            final_rows[i]["values"].extend(prod)
 
         # Reduce
         for row in final_rows:
@@ -155,13 +155,34 @@ if __name__ == "__main__":
     input2_table2 = os.path.join(
         os.path.dirname(__file__), "sample/natural_join/input2_table2.txt"
     )
+    # input1_table1 = os.path.join(
+    #     os.path.dirname(__file__), "sample/natural_join/inputs/input1_table1.txt"
+    # )
+    # input1_table2 = os.path.join(
+    #     os.path.dirname(__file__), "sample/natural_join/inputs/input1_table2.txt"
+    # )
+    # input1_table3 = os.path.join(
+    #     os.path.dirname(__file__), "sample/natural_join/inputs/input1_table3.txt"
+    # )
+    # input2_table1 = os.path.join(
+    #     os.path.dirname(__file__), "sample/natural_join/inputs/input2_table1.txt"
+    # )
+    # input2_table2 = os.path.join(
+    #     os.path.dirname(__file__), "sample/natural_join/inputs/input2_table2.txt"
+    # )
+    # input2_table3 = os.path.join(
+    #     os.path.dirname(__file__), "sample/natural_join/inputs/input2_table3.txt"
+    # )
     manager = Manager(
         "natural_join",
         config,
         [],
         NaturalJoinMapper,
         NaturalJoinReducer,
-        [[input1_table1, input1_table2], [input2_table1, input2_table2]],
+        [
+            [input1_table1, input1_table2],
+            [input2_table1, input2_table2],
+        ],
     )
     for i in range(len(manager.reducers)):
         port = manager.find_free_port()
